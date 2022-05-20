08c9b87f4300b00cc9ea484214a8ad1e1429f9ca
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-20 16:55:26 -0300

    component profile usando githubHooks

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import useGithub from "../../hooks/github-hooks";
 import * as S from "./styled";
 
 const Profile = () => {
+  const { githubState } = useGithub();
   return (
     <S.Wrapper>
       <S.WrapperImage
@@ -11,30 +13,34 @@ const Profile = () => {
       />
       <S.WrapperInfoUser>
         <div>
-          <h1>Emerson Carvalho</h1>
+          <h1>{githubState.user.name}</h1>
           <S.WrapperUsername>
             <h3>Username: </h3>
             <a
-              href="https://github.com/brecarv"
+              href={githubState.user.html_url}
               target="_blank"
               rel="noopener noreferrer"
             >
-              brecarv
+              {githubState.user.login}
             </a>
           </S.WrapperUsername>
         </div>
         <S.WrapperStatusCount>
           <div>
             <h4>Followers</h4>
-            <span>10</span>
-          </div>
-          <div>
-            <h4>Starreds</h4>
-            <span>10</span>
+            <span>{githubState.user.followers}</span>
           </div>
           <div>
             <h4>Followings</h4>
-            <span>10</span>
+            <span>{githubState.user.following}</span>
+          </div>
+          <div>
+            <h4>Gists</h4>
+            <span>{githubState.user.publicGists}</span>
+          </div>
+          <div>
+            <h4>Repos</h4>
+            <span>{githubState.user.publicRepos}</span>
           </div>
         </S.WrapperStatusCount>
       </S.WrapperInfoUser>
