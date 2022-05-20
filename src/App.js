52856b04e4f8c8b40a8d74e6dada9fc875c35b48
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-20 10:29:51 -0300

    app esqueleto mock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from "react";
+import Layout from "./components/layout";
 
 function App() {
-  return <div className="App">github-api</div>;
+  return (
+    <main>
+      <Layout>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/8569519?v=4"
+            aria-hidden
+            alt="Picture of user"
+          />
+          <h1>Emerson Carvalho</h1>
+          <h3>Username: </h3>
+          <span>brecarv</span>
+          <div>
+            <div>
+              <h4>Followers</h4>
+              <span>10</span>
+            </div>
+            <div>
+              <h4>Starreds</h4>
+              <span>10</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>10</span>
+            </div>
+          </div>
+        </div>
+
+        <div>Repositories</div>
+
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  );
 }
 
 export default App;
