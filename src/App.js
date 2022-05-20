659ced822796f26878a43ce996bbc003d707ecff
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-20 16:54:38 -0300

    githubProvider add no App

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Layout from "./components/layout";
 import Profile from "./components/profile";
 import Repositories from "./components/repositories";
 import { ResetCSS } from "./global/resetCSS";
+import GithubProvider from "./providers/github-provider";
 
 const App = () => {
   return (
     <main>
-      <ResetCSS />
-      <Layout>
-        <Profile />
-        <Repositories />
-      </Layout>
+      <GithubProvider>
+        <ResetCSS />
+        <Layout>
+          <Profile />
+          <Repositories />
+        </Layout>
+      </GithubProvider>
     </main>
   );
 };
