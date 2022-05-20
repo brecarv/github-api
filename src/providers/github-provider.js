2ece6a4761190682455860f47702f373dba60c20
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-20 16:53:18 -0300

    provider implementado usando createContext e useState

---
+++ src/providers/github-provider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useState } from "react";
+
+export const GithubContext = createContext({
+  user: {},
+  repositories: [],
+  starred: [],
+});
+
+const GithubProvider = ({ children }) => {
+  const [githubState, setGithubState] = useState({
+    user: {
+      login: undefined,
+      name: undefined,
+      html_url: undefined,
+      blog: undefined,
+      company: undefined,
+      location: undefined,
+      followers: 0,
+      following: 0,
+      publicGists: 0,
+      publicRepos: 0,
+    },
+    repositories: [],
+    starred: [],
+  });
+  const contextValue = {
+    githubState,
+  };
+  return (
+    <GithubContext.Provider value={contextValue}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubProvider;
