b060f05def4052f32931f2dd10745417f1b0bf2f
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-20 15:25:18 -0300

    component repositories criado

---
+++ src/components/repositories/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import * as S from "./styled";
+
+const Repositories = () => {
+  return (
+    <S.WrapperTabs
+      selectedTabClassName="is-selected"
+      selectedTabPanelClassName="is-selected"
+    >
+      <S.WrapperTabList>
+        <S.WrapperTab>Repositories</S.WrapperTab>
+        <S.WrapperTab>Starred</S.WrapperTab>
+      </S.WrapperTabList>
+      <S.WrapperTabPanel>Panel Repositories</S.WrapperTabPanel>
+      <S.WrapperTabPanel>Panel Starred</S.WrapperTabPanel>
+    </S.WrapperTabs>
+  );
+};
+
+export default Repositories;
