e0c6a9add39b4f5e9154d24053c8b5bf418c2e99
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-21 14:10:31 -0300

    header: busca implementada

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+
+import Header from "../header";
 import * as S from "./styled";
 
 const Layout = ({ children }) => {
   return (
     <S.WrapperLayout>
-      <header>Header</header>
+      <Header />
       {children}
     </S.WrapperLayout>
   );
