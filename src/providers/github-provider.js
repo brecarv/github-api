06073945203f5828be03c36de58945c80566e59a
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-21 14:04:02 -0300

    githubProvider consumindo usuario

--- src/providers/github-provider.js
+++ src/providers/github-provider.js
@@ -1,6 +1,8 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useCallback, useState } from "react";
+import api from "../services/api";
 
 export const GithubContext = createContext({
+  loading: false,
   user: {},
   repositories: [],
   starred: [],
@@ -8,23 +10,62 @@ export const GithubContext = createContext({
 
 const GithubProvider = ({ children }) => {
   const [githubState, setGithubState] = useState({
+    hasUser: false,
+    loading: false,
     user: {
       login: undefined,
       name: undefined,
+      avatar_url: undefined,
       html_url: undefined,
       blog: undefined,
       company: undefined,
       location: undefined,
       followers: 0,
       following: 0,
-      publicGists: 0,
-      publicRepos: 0,
+      public_gists: 0,
+      public_repos: 0,
     },
     repositories: [],
     starred: [],
   });
+
+  const getUser = (username) => {
+    setGithubState((prevState) => ({
+      ...prevState,
+      loading: !prevState.loading,
+    }));
+    api
+      .get(`users/${username}`)
+      .then(({ data }) => {
+        setGithubState((prevState) => ({
+          ...prevState,
+          hasUser: true,
+          user: {
+            login: data.login,
+            name: data.name,
+            avatar_url: data.avatar_url,
+            html_url: data.html_url,
+            blog: data.blog,
+            company: data.company,
+            location: data.location,
+            followers: data.followers,
+            following: data.following,
+            public_gists: data.public_gists,
+            public_repos: data.public_repos,
+          },
+        }));
+      })
+      .finally(() => {
+        setGithubState((prevState) => ({
+          ...prevState,
+          loading: !prevState.loading,
+        }));
+      });
+  };
+
   const contextValue = {
     githubState,
+    getUser: useCallback((username) => getUser(username), []),
   };
   return (
     <GithubContext.Provider value={contextValue}>
