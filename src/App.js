7aef8cea220aab6597348c59809d2b5625f40584
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-22 11:30:47 -0300

    add NoSearch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
 import Profile from "./components/profile";
 import Repositories from "./components/repositories";
 import useGithub from "./hooks/github-hooks";
@@ -20,7 +21,7 @@ const App = () => {
           )}
         </>
       ) : (
-        <div>Nenhum usuário pesquisado...</div>
+        <NoSearch />
       )}
     </Layout>
   );
